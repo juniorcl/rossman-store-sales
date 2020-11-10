da3f9f97e40b4d38eebdc58b012ef515231f8392
juniorcl <clebiojuniorcj@gmail.com>
2020-11-09 21:00:47 -0300

    finished model deploy

--- api/handler.py
+++ api/handler.py
@@ -4,7 +4,7 @@ from flask             import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # loading model
-model = load("../models/model_rossmann.joblib")
+model = load("models/model_rossmann.joblib")
 
 # initialize API
 app = Flask( __name__ )
@@ -24,16 +24,16 @@ def rossmann_predict():
         pipeline = Rossmann()
         
         # data cleaning
-        df1 = pipeline.data_cleaning( test_raw )
+        df1 = pipeline.data_cleaning(test_raw)
         
         # feature engineering
-        df2 = pipeline.feature_engineering( df1 )
+        df2 = pipeline.feature_engineering(df1)
         
         # data preparation
-        df3 = pipeline.data_preparation( df2 )
+        df3 = pipeline.data_preparation(df2)
         
         # prediction
-        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        df_response = pipeline.get_prediction(model, test_raw, df3)
         
         return df_response
         
@@ -42,4 +42,4 @@ def rossmann_predict():
         return Response("{}", status=200, mimetype="application/json")
 
 if __name__ == '__main__':
-    app.run( '0.0.0.0' )
+    app.run('0.0.0.0')
